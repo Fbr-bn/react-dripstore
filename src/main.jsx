e89e4cb2d71f9ab77e4ab5511a61a27ea9cdef51
Natan Uderson <udersonn39@gmail.com>
2025-05-27 10:44:38 -0300

    Tinha deixado um erro. Corrigido

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx";
-import FormAccount from './pages/FormAccount.jsx';
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <FormAccount/>
+    <App />
+    
     
   </StrictMode>
 )
