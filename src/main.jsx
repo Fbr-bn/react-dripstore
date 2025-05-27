6ce15d8613454d317b09484598c9c3abbc3fb0f4
fernando <fneto48@gmail.com>
2025-05-27 12:10:21 -0300

    FINALIZADO

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ProductDetails from "./pages/ProductDetails.jsx";
-import { Routes, Route } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-        <Route path="/product-details" element={<ProductDetails />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-export default App;
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ProductImages from "../components/ProductImages";
 import ProductInfo from "../components/ProductInfo";
 import RelatedProducts from "../components/RelatedProducts";
-import LayoutWrapper from "../components/LayoutWrapper";
+
 
 export default function ProductDetails() {
   return (
-    <LayoutWrapper>
+ 
     <div className="md:pt-0 bg-light-gray-1 pb-15 bg-gray-50">
       <div className="container mx-auto">
         <div className="mr-8 text-xs text-gray-500 mb-4 pt-3 px-2">
@@ -20,6 +20,6 @@ export default function ProductDetails() {
         <RelatedProducts />
       </div>
     </div>
-    </LayoutWrapper>
+ 
   );
 }
