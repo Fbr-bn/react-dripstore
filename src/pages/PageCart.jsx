5a2775440e7b642a1ea37975c3d6d8ee70f2d6ea
lucas <lkaue211@gmail.com>
2025-05-27 16:38:50 -0300

    Criei a página meu carrinho

---
+++ src/pages/PageCart.jsx
@@ -0,0 +1,99 @@
+export default function PageCart(){
+    
+    <div>
+    <LayoutWrapper>
+<div className="mt-4 max-w-7xl mx-auto hidden sm:block">
+          <div className="flex justify-between items-center mb-4">
+            <p className="text-sm text-gray-600">Produtos Relacionados</p>
+            <a href="#" className="text-sm text-blue-500 hover:underline"
+              >Ver todos →</a
+            >
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-5 md:grid-cols-5 gap-4">
+            <div>
+              <div className="bg-white p-4 relative">
+                <span
+                className="top-2 left-2 bg-lime-200 text-green-700 text-xs font-semibold px-4 py-1 rounded-full"
+                >30% OFF</span
+                >
+                <img src="Layer 1aa 2.png" alt="Tênis" className="mx-auto" />
+              </div>
+              <div className="bg-[#F9F8FE] p-4">
+                <p className="text-gray-400 text-xs">Tênis</p>
+                <div
+                  className="text-sm font-semibold max-[375px]:truncate max-[375px]:overflow-hidden max-[375px]:whitespace-nowrap"
+                >
+                  K-Swiss V8 - Masculino
+                </div>
+                <div className="text-sm">
+                  <span className="line-through text-gray-400">$200</span>
+                  <span className="text-black font-bold">$100</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="bg-white p-4 relative">
+                <span
+                  className="top-2 left-2 text-xs font-semibold px-4 py-1 rounded-full"
+                ></span>
+                <img src="Layer 1aa 2.png" alt="Tênis" className="mx-auto" />
+              </div>
+              <div className="bg-[#F9F8FE] p-4">
+                <p className="text-gray-400 text-xs">Tênis</p>
+                <div
+                  className="text-sm font-semibold max-[375px]:truncate max-[375px]:overflow-hidden max-[375px]:whitespace-nowrap"
+                >
+                  K-Swiss V8 - Masculino
+                </div>
+                <div className="text-sm">
+                  <span className="line-through text-gray-400">$200</span>
+                  <span className="text-black font-bold">$100</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="bg-white p-4 relative">
+                <span className="top-2 left-2 px-4 py-1 rounded-full"></span>
+                <img src="Layer 1aa 2.png" alt="Tênis" className="mx-auto" />
+              </div>
+              <div className="bg-[#F9F8FE] p-4">
+                <p className="text-gray-400 text-xs">Tênis</p>
+                <div
+                  className="text-sm font-semibold max-[375px]:truncate max-[375px]:overflow-hidden max-[375px]:whitespace-nowrap"
+                >
+                  K-Swiss V8 - Masculino
+                </div>
+                <div className="text-sm">
+                  <span className="line-through text-gray-400">$200</span>
+                  <span className="text-black font-bold">$100</span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="bg-white p-4 relative">
+                <span className="top-2 left-2 px-4 py-1 rounded-full"></span>
+                <img src="Layer 1aa 2.png" alt="Tênis" className="mx-auto" />
+              </div>
+              <div className="bg-[#F9F8FE] p-4">
+                <p className="text-gray-400 text-xs">Tênis</p>
+                <div
+                  className="text-sm font-semibold max-[375px]:truncate max-[375px]:overflow-hidden max-[375px]:whitespace-nowrap"
+                >
+                  K-Swiss V8 - Masculino
+                </div>
+                <div className="text-sm">
+                  <span className="line-through text-gray-400">$200</span>
+                  <span className="text-black font-bold">$100</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </LayoutWrapper>
+
+</div>
+};
