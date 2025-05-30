61eacf1661d9675193934e40ce3c79520a2c7818
Natan Uderson <udersonn39@gmail.com>
2025-05-30 09:58:13 -0300

    subindo a a pagina de confirmação de compras

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductDetails from "./pages/ProductDetails";
 import MyOrders from "./pages/MyOrders";
+import PageConfirmation from "./pages/PageConfirmation";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/product-details" element={<ProductDetails />} />
         <Route path="/my-orders" element={<MyOrders />} />
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
