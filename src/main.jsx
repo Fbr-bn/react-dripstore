72b19eef5e0e742ef34125d11467b72af0b868c2
fernando <fneto48@gmail.com>
2025-05-30 10:38:59 -0300

    Adicionei as  rotas das páginas de login e criar conta

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductDetails from "./pages/ProductDetails";
 import MyOrders from "./pages/MyOrders";
+import AccountLogin from "./pages/AccountLogin";
+import FormAccount from "./pages/FormAccount";
+import AccoutCreate from "./pages/AccountCreate";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<AccountLogin />} />
+        <Route path="/create" element={<AccoutCreate />} />
+        <Route path="/form-create" element={<FormAccount />} />
         <Route path="/product-details" element={<ProductDetails />} />
         <Route path="/my-orders" element={<MyOrders />} />
       </Routes>
