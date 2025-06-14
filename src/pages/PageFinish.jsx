89abf3503e613d709bbd7ca970f3d8962ae8bb25
nilton2305 <nilton.bezerra.barros@gmail.com>
2025-06-14 00:22:11 -0300

    subindo page-finish e o componente ReciboCompra

---
+++ src/pages/PageFinish.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutWrapper from "../components/LayoutWrapper"; // ajuste o caminho se necessário
+import ReciboCompra from "../components/ReciboCompra";
+
+const PageFinish = () => {
+  return (
+    <LayoutWrapper>
+      <ReciboCompra />
+    </LayoutWrapper>
+  );
+};
+
+export default PageFinish;
